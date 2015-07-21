8330f41aed4ecb7dd7c4dbfc64c6b83a6bba4788
Rob Williams <rob@workshoptwelve.com>
2015-07-21 17:00:07 -0400

    Changed functionality so that instances of Pollable are (generally) added.

--- libraries/WS12/Pollable.h
+++ libraries/WS12/Pollable.h
@@ -3,9 +3,22 @@
 
 #include <Arduino.h>
 
+#ifndef _POLLABLE_COUNT
+    #define _POLLABLE_COUNT 10
+#endif
+
 class Pollable {
   public:
+    Pollable(bool add = true);
+    virtual ~Pollable();
+
     virtual void poll(uint32_t now) = 0;
+    
+    static void pollAll();
+
+  private:
+    static void addPollable(Pollable * pollable);
+    static void removePollable(Pollable * pollable);
 };
 
 #endif
