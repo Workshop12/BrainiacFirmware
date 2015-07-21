63c9bf7f7b6808719c66e316494cd0318bc49704
Rob Williams <rob@workshoptwelve.com>
2015-07-21 17:04:12 -0400

    Moving constants.

--- relayBoard/fourByFour/global.h
+++ relayBoard/fourByFour/global.h
@@ -10,7 +10,15 @@
 #include "RelayChannel.h"
 #include "PolarisedChannel.h"
 
+#include <Strings.h>
+
 extern RelayChannel switchChannels[SWITCH_CHANNEL_COUNT];
 extern PolarisedChannel directedChannels[DIRECTED_CHANNEL_COUNT];
+
+extern const PStr command_enum;
+extern const PStr command_set;
+
+extern const PStr product_details;
+
 
 #endif
